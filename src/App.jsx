c93877deab2fb05ff23293cf665e294271de42d2
rzzza_rehan <rhn.raza121@gmail.com>
2024-09-08 23:16:14 +0530

    consume and link the context

--- src/App.jsx
+++ src/App.jsx
@@ -65,9 +65,12 @@ function App() {
       };
     });
   }
-
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart 
+  }
   return (
-    <CartContext.Provider>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
